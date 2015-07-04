4b8b7c7f02f69cb117ba3689dd243f125876eef1
ousttrue <ousttrue@gmail.com>
2015-07-05 08:54:10 +0900

    complete

--- BvhSample/Program.cs
+++ BvhSample/Program.cs
@@ -40,7 +40,7 @@ namespace BvhSample
 
         public override string ToString()
         {
-            return String.Format("{0}[{1}, {2}, {3}]{4}", Name, Offset.X, Offset.Y, Offset.Z, String.Join("", Channels));
+            return String.Format("{0}[{1}, {2}, {3}]{4}", Name, Offset.X, Offset.Y, Offset.Z, String.Join(", ", Channels));
         }
 
         public void Traverse(Action<Node, int> pred, int level = 0)
@@ -56,15 +56,61 @@ namespace BvhSample
 
     static class BvhParser
     {
-        public static Parser<Node> Parser
-        {
-            get
-            {
-                return from herarchy in Parse.String("HIERARCHY")
-                       select new Node("dummy", new Vector3());
+        public static Parser<String> Exponent = from _ in Parse.Char('E')
+                                               from sign in Parse.Chars("+-")
+                                               from num in Parse.Number
+                                               select String.Format("E{0}{1}", sign, num);
 
-            }
+        public static Parser<Single> FloatEx = from negative in Parse.Char('-').Optional().Select(x => x.IsDefined ? x.Get().ToString() : "")
+                                               from num in Parse.Decimal
+                                               from exponent in Exponent.Optional().Select(x => x.IsDefined ? x.Get() : "")
+                                               select Convert.ToSingle(negative+num+exponent);
+
+        public static Parser<Vector3> Offset = from _ in Parse.String("OFFSET").Token()
+                                               from x in FloatEx.Token().Select(x => Convert.ToSingle(x))
+                                               from y in FloatEx.Token().Select(x => Convert.ToSingle(x))
+                                               from z in FloatEx.Token().Select(x => Convert.ToSingle(x))
+                                               select new Vector3 {
+                                                   X=x,
+                                                   Y=y,
+                                                   Z=z
+                                               };
+
+        public static Parser<IEnumerable<ChannelType>> Channels = from _ in Parse.String("CHANNELS").Token()
+                                                                  from n in Parse.Number.Select(x => Convert.ToInt32(x))
+                                                                  from channels in Parse.String("Xposition").Token().Return(ChannelType.Xposition)
+                                                                      .Or(Parse.String("Yposition").Token().Return(ChannelType.Yposition))
+                                                                      .Or(Parse.String("Zposition").Token().Return(ChannelType.Zposition))
+                                                                      .Or(Parse.String("Xrotation").Token().Return(ChannelType.Xrotation))
+                                                                      .Or(Parse.String("Yrotation").Token().Return(ChannelType.Yrotation))
+                                                                      .Or(Parse.String("Zrotation").Token().Return(ChannelType.Zrotation)).Many()
+                                                                  select channels
+                                                                    ;
+
+        public static Parser<Node> EndSite = from _ in Parse.String("End Site").Token()
+                                             from open in Parse.Char('{').Token()
+                                             from offset in Offset
+                                             from close in Parse.Char('}').Token()
+                                             select new Node("EndSite", offset);
+
+        public static Parser<Node> Node(String prefix)
+        {
+            return from _ in Parse.String(prefix).Token()
+                   from name in Parse.LetterOrDigit.Many().Token().Text()
+                   from open in Parse.Char('{').Token()
+                   from offset in Offset
+                   from channels in Channels
+                   from children in Node("JOINT").AtLeastOnce().Or(EndSite
+                        // 型をIEnumerable<Node>にそろえる
+                        .Select(x => new Node[] { x }))
+                   from close in Parse.Char('}').Token()
+                   select new Node(name, offset, channels, children)
+                                        ;
         }
+
+        public static Parser<Node> Parser = from hierarchy in Parse.String("HIERARCHY").Token()
+                                           from root in Node("ROOT")
+                                           select root;
     }
 
     class Program
@@ -72,10 +118,9 @@ namespace BvhSample
         static void Main(string[] args)
         {
             var path = args.First();
-            var bvhtext=File.ReadAllText(path);
+            var bvhtext = File.ReadAllText(path);
 
-            var parser = BvhParser.Parser;
-            var root = parser.Parse(bvhtext);
+            var root = BvhParser.Parser.Parse(bvhtext);
 
             root.Traverse((node, level) => {
                 Console.WriteLine(String.Format("{0}{1}"
